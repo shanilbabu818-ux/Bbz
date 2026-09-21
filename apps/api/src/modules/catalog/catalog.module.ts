16af75af5624c72bb865ba7435ecb430fb2548ef
Bbz <shanilbabu818@gmail.com>
2026-09-22 05:14:41 +0530

    feat: register PostgreSQL catalog service

--- apps/api/src/modules/catalog/catalog.module.ts
+++ apps/api/src/modules/catalog/catalog.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CatalogController } from './catalog.controller';
 import { CatalogService } from './catalog.service';
+import { CatalogDbService } from './catalog-db.service';
 
-@Module({ controllers: [CatalogController], providers: [CatalogService], exports: [CatalogService] })
+@Module({ controllers: [CatalogController], providers: [CatalogService, CatalogDbService], exports: [CatalogService, CatalogDbService] })
 export class CatalogModule {}
