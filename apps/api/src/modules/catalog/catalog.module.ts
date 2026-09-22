0da5868210db5a362fcda56642a6ca709b273259
Bbz <shanilbabu818@gmail.com>
2026-09-23 03:15:19 +0530

    feat(catalog): register product service

--- apps/api/src/modules/catalog/catalog.module.ts
+++ apps/api/src/modules/catalog/catalog.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CatalogController } from './catalog.controller';
+import { CatalogService } from './catalog.service';
 
-@Module({ controllers: [CatalogController] })
+@Module({ controllers: [CatalogController], providers: [CatalogService] })
 export class CatalogModule {}
