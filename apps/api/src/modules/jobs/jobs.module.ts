2ae9d25e5776813b7e1d35f367fa0f0d252e7591
Bbz <shanilbabu818@gmail.com>
2026-09-23 03:21:57 +0530

    feat(workers): register jobs module

---
+++ apps/api/src/modules/jobs/jobs.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+import { AiScoringService } from './ai-scoring.service';
+
+@Module({ providers: [AiScoringService], exports: [AiScoringService] })
+export class JobsModule {}
