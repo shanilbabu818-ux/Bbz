3a8aed734cc1cad1c205f0bef9b174a22122731f
Bbz <shanilbabu818@gmail.com>
2026-09-23 03:22:33 +0530

    feat(workers): register consumer and scoring services

--- apps/api/src/modules/jobs/jobs.module.ts
+++ apps/api/src/modules/jobs/jobs.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AiScoringService } from './ai-scoring.service';
+import { EventConsumerService } from './event-consumer.service';
 
-@Module({ providers: [AiScoringService], exports: [AiScoringService] })
+@Module({
+  providers: [AiScoringService, EventConsumerService],
+  exports: [AiScoringService, EventConsumerService],
+})
 export class JobsModule {}
