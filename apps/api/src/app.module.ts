34c2339033032fbc917d0e3c12e491d4de18b0de
Bbz <shanilbabu818@gmail.com>
2026-09-23 03:20:40 +0530

    feat(outbox): enable outbox module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { CatalogModule } from './modules/catalog/catalog.module';
+import { OutboxModule } from './modules/outbox/outbox.module';
 import { SuppliersModule } from './modules/suppliers/suppliers.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), HealthModule, AuthModule, CatalogModule, SuppliersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), HealthModule, AuthModule, CatalogModule, OutboxModule, SuppliersModule],
 })
 export class AppModule {}
