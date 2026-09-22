f89b41ae93d55684b39d9df8443c7f32512f7ade
Bbz <shanilbabu818@gmail.com>
2026-09-23 03:37:45 +0530

    fix(suppliers): remove self-import from module

--- apps/api/src/modules/suppliers/suppliers.module.ts
+++ apps/api/src/modules/suppliers/suppliers.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ShopifyModule } from '../shopify/shopify.module';
-import { SuppliersModule } from './suppliers.module';
 
 @Module({ imports: [ShopifyModule], exports: [ShopifyModule] })
 export class SuppliersModule {}
